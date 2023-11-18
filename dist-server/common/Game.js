58d42b88751ebf35b71094068b3ffd6adad65c1e
Yiyi Xu <xuyiyi0516@gmail.com>
2023-11-18 16:36:19 -0500

    working basic multiplayer pong

--- dist-server/common/Game.js
+++ dist-server/common/Game.js
@@ -62,6 +62,75 @@ function (_DynamicObject) {
   }]);
 
   return YourGameObject;
+}(_lanceGg.DynamicObject);
+
+var PADDING = 20;
+var WIDTH = 400;
+var HEIGHT = 400;
+var PADDLE_WIDTH = 10;
+var PADDLE_HEIGHT = 50;
+
+var Paddle =
+/*#__PURE__*/
+function (_DynamicObject2) {
+  _inherits(Paddle, _DynamicObject2);
+
+  function Paddle(gameEngine, options, props) {
+    _classCallCheck(this, Paddle);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Paddle).call(this, gameEngine, options, props));
+  }
+
+  _createClass(Paddle, [{
+    key: "syncTo",
+    value: function syncTo(other) {
+      _get(_getPrototypeOf(Paddle.prototype), "syncTo", this).call(this, other);
+
+      this.health = other.health;
+    }
+  }], [{
+    key: "netScheme",
+    get: function get() {
+      return Object.assign({
+        health: {
+          type: _lanceGg.BaseTypes.TYPES.INT16
+        }
+      }, _get(_getPrototypeOf(Paddle), "netScheme", this));
+    }
+  }]);
+
+  return Paddle;
+}(_lanceGg.DynamicObject);
+
+var Ball =
+/*#__PURE__*/
+function (_DynamicObject3) {
+  _inherits(Ball, _DynamicObject3);
+
+  function Ball(gameEngine, options, props) {
+    _classCallCheck(this, Ball);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Ball).call(this, gameEngine, options, props));
+  } // avoid gradual synchronization of velocity
+
+
+  _createClass(Ball, [{
+    key: "syncTo",
+    value: function syncTo(other) {
+      _get(_getPrototypeOf(Ball.prototype), "syncTo", this).call(this, other);
+    }
+  }, {
+    key: "bending",
+    get: function get() {
+      return {
+        velocity: {
+          percent: 0.0
+        }
+      };
+    }
+  }]);
+
+  return Ball;
 }(_lanceGg.DynamicObject); // /////////////////////////////////////////////////////////
 //
 // GAME ENGINE
@@ -84,19 +153,19 @@ function (_GameEngine) {
       gameEngine: _assertThisInitialized(_this)
     }); // common code
 
-    _this.on('postStep', _this.gameLogic.bind(_assertThisInitialized(_this))); // server-only code
+    _this.on("postStep", _this.gameLogic.bind(_assertThisInitialized(_this))); // server-only code
 
 
-    _this.on('server__init', _this.serverSideInit.bind(_assertThisInitialized(_this)));
+    _this.on("server__init", _this.serverSideInit.bind(_assertThisInitialized(_this)));
 
-    _this.on('server__playerJoined', _this.serverSidePlayerJoined.bind(_assertThisInitialized(_this)));
+    _this.on("server__playerJoined", _this.serverSidePlayerJoined.bind(_assertThisInitialized(_this)));
 
-    _this.on('server__playerDisconnected', _this.serverSidePlayerDisconnected.bind(_assertThisInitialized(_this))); // client-only code
+    _this.on("server__playerDisconnected", _this.serverSidePlayerDisconnected.bind(_assertThisInitialized(_this))); // client-only code
 
 
-    _this.on('client__rendererReady', _this.clientSideInit.bind(_assertThisInitialized(_this)));
+    _this.on("client__rendererReady", _this.clientSideInit.bind(_assertThisInitialized(_this)));
 
-    _this.on('client__draw', _this.clientSideDraw.bind(_assertThisInitialized(_this)));
+    _this.on("client__draw", _this.clientSideDraw.bind(_assertThisInitialized(_this)));
 
     return _this;
   }
@@ -104,15 +173,71 @@ function (_GameEngine) {
   _createClass(Game, [{
     key: "registerClasses",
     value: function registerClasses(serializer) {
-      serializer.registerClass(YourGameObject);
+      serializer.registerClass(Paddle);
+      serializer.registerClass(Ball);
     }
   }, {
     key: "gameLogic",
-    value: function gameLogic() {}
+    value: function gameLogic() {
+      var paddles = this.world.queryObjects({
+        instanceType: Paddle
+      });
+      var ball = this.world.queryObject({
+        instanceType: Ball
+      });
+      if (!ball || paddles.length !== 2) return; // CHECK LEFT EDGE:
+
+      if (ball.position.x <= PADDING + PADDLE_WIDTH && ball.position.y >= paddles[0].y && ball.position.y <= paddles[0].position.y + PADDLE_HEIGHT && ball.velocity.x < 0) {
+        // ball moving left hit player 1 paddle
+        ball.velocity.x *= -1;
+        ball.position.x = PADDING + PADDLE_WIDTH + 1;
+      } else if (ball.position.x <= 0) {
+        // ball hit left wall
+        ball.velocity.x *= -1;
+        ball.position.x = 0;
+        console.log("player 2 scored");
+        paddles[0].health--;
+      } // CHECK RIGHT EDGE:
+
+
+      if (ball.position.x >= WIDTH - PADDING - PADDLE_WIDTH && ball.position.y >= paddles[1].position.y && ball.position.y <= paddles[1].position.y + PADDLE_HEIGHT && ball.velocity.x > 0) {
+        // ball moving right hits player 2 paddle
+        ball.velocity.x *= -1;
+        ball.position.x = WIDTH - PADDING - PADDLE_WIDTH - 1;
+      } else if (ball.position.x >= WIDTH) {
+        // ball hit right wall
+        ball.velocity.x *= -1;
+        ball.position.x = WIDTH - 1;
+        console.log("player 1 scored");
+        paddles[1].health--;
+      } // ball hits top or bottom edge
+
+
+      if (ball.position.y <= 0) {
+        ball.position.y = 1;
+        ball.velocity.y *= -1;
+      } else if (ball.position.y >= HEIGHT) {
+        ball.position.y = HEIGHT - 1;
+        ball.velocity.y *= -1;
+      }
+    }
   }, {
     key: "processInput",
     value: function processInput(inputData, playerId) {
-      _get(_getPrototypeOf(Game.prototype), "processInput", this).call(this, inputData, playerId);
+      _get(_getPrototypeOf(Game.prototype), "processInput", this).call(this, inputData, playerId); // get the player paddle tied to the player socket
+
+
+      var playerPaddle = this.world.queryObject({
+        playerId: playerId
+      });
+
+      if (playerPaddle) {
+        if (inputData.input === "up") {
+          playerPaddle.position.y -= 5;
+        } else if (inputData.input === "down") {
+          playerPaddle.position.y += 5;
+        }
+      }
     } // /////////////////////////////////////////////////////////
     //
     // SERVER ONLY CODE
@@ -121,13 +246,46 @@ function (_GameEngine) {
 
   }, {
     key: "serverSideInit",
-    value: function serverSideInit() {}
+    value: function serverSideInit() {
+      // create the paddle objects
+      this.addObjectToWorld(new Paddle(this, null, {
+        position: new _lanceGg.TwoVector(PADDING, 0)
+      }));
+      this.addObjectToWorld(new Paddle(this, null, {
+        position: new _lanceGg.TwoVector(WIDTH - PADDING, 0)
+      }));
+      this.addObjectToWorld(new Ball(this, null, {
+        position: new _lanceGg.TwoVector(WIDTH / 2, HEIGHT / 2),
+        velocity: new _lanceGg.TwoVector(2, 2)
+      }));
+    } // attach newly connected player to next available paddle
+
   }, {
     key: "serverSidePlayerJoined",
-    value: function serverSidePlayerJoined(ev) {}
+    value: function serverSidePlayerJoined(ev) {
+      var paddles = this.world.queryObjects({
+        instanceType: Paddle
+      });
+
+      if (paddles[0].playerId === 0) {
+        paddles[0].playerId = ev.playerId;
+      } else if (paddles[1].playerId === 0) {
+        paddles[1].playerId = ev.playerId;
+      }
+    }
   }, {
     key: "serverSidePlayerDisconnected",
-    value: function serverSidePlayerDisconnected(ev) {} // /////////////////////////////////////////////////////////
+    value: function serverSidePlayerDisconnected(ev) {
+      var paddles = this.world.queryObjects({
+        instanceType: Paddle
+      });
+
+      if (paddles[0].playerId === ev.id) {
+        paddles[0].playerId = 0;
+      } else if (paddles[1].playerId === ev.id) {
+        paddles[1].playerId = 0;
+      }
+    } // /////////////////////////////////////////////////////////
     //
     // CLIENT ONLY CODE
     //
@@ -135,10 +293,36 @@ function (_GameEngine) {
 
   }, {
     key: "clientSideInit",
-    value: function clientSideInit() {}
+    value: function clientSideInit() {
+      this.controls = new _lanceGg.KeyboardControls(this.renderer.clientEngine);
+      this.controls.bindKey('up', 'up', {
+        repeat: true
+      });
+      this.controls.bindKey('down', 'down', {
+        repeat: true
+      });
+    }
   }, {
     key: "clientSideDraw",
-    value: function clientSideDraw() {}
+    value: function clientSideDraw() {
+      function updateEl(el, obj) {
+        var health = obj.health > 0 ? obj.health : 15;
+        el.style.top = obj.position.y + 10 + 'px';
+        el.style.left = obj.position.x + 'px';
+        el.style.background = "#ff".concat(health.toString(16), "f").concat(health.toString(16), "f");
+      }
+
+      var paddles = this.world.queryObjects({
+        instanceType: Paddle
+      });
+      var ball = this.world.queryObject({
+        instanceType: Ball
+      });
+      if (!ball || paddles.length !== 2) return;
+      updateEl(document.querySelector('.ball'), ball);
+      updateEl(document.querySelector('.paddle1'), paddles[0]);
+      updateEl(document.querySelector('.paddle2'), paddles[1]);
+    }
   }]);
 
   return Game;
