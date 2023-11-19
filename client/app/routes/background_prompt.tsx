a1b5c9a2d3f1c7d8e133cc02b57ea687ca4d1521
Yiyi Xu <xuyiyi0516@gmail.com>
2023-11-19 08:32:58 -0500

    good stuff

--- client/app/routes/background_prompt.tsx
+++ client/app/routes/background_prompt.tsx
@@ -11,7 +11,8 @@ const TextBoxComponent = () => {
     };
 
     const handleSubmit = async () => {
-        const prompt = " A densely packed cartoonic 'where's waldo?' scene with a " + text + "background without Waldo";
+        // const prompt = " A scene with a " + text + "background in a 'Where's Waldo' style with many small cartoonish characters in the scene but WITHOUT having a Waldo character anywhere in the scene";
+        const prompt = "wheres waldo style childrens book," + text;
         setSubmittedText(text);
         const res = await fetch(`http://localhost:3001/generate_waldo?prompt=${prompt}`, {
             method: 'GET',
@@ -19,7 +20,6 @@ const TextBoxComponent = () => {
                 'Content-Type': 'application/json',
             },
         }).then(response => response.json()).catch(error => console.error('Error:', error));
-        console.log(res.img_url);
         setImgUrl(res.img_url);
     };
 
